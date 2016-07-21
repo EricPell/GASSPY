f9070544d8e9209deec639df98a7facf296a9e3d
Eric Pellegrini <Eric Pellegrini>
2016-07-21 17:35:09 +0200

    Changed avgEmissivity type to string.

--- combine-ems.pl
+++ combine-ems.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+# OneDrive/Research/ISM-models/SILCC/bin/optimal_post_processing
 
 $prefix = $ARGV[0];
 @FILES = glob($prefix."*.ems");
@@ -21,7 +22,7 @@ for $file (@FILES){
 	# one zone models this is simply the second entry in data.
 
 	# Calculate average of emissivity file
-	@AverageData = &avgEmissivty(\@data);
+	$AverageData = &avgEmissivty(\@data);
 
 	# Join ID number and emissivity into a string.
 	my $string = join("\t",($ID,@AverageData));
@@ -53,6 +54,39 @@ close(OUT)
 
 
 sub avgEmissivty{
-    my @array = @{$_[0]};
+    # dereference array containing emissivity data
+    my @array_rows = @{$_[0]};
+
+    # Initialize depth at the face of the cloud
+    $r_old = 0.0;
+
+    # Loop over each row
     
+    #Read number of header entries, split by tab
+    my @array = split("\t",$array_rows[0]);
+
+    # Initialize SumProduct and AverageEmissivity arrays
+    @SumProduct_dr_emissivity = (0) x scalar @array;
+    @AverageEmissivity = (0) x scalar @array;
+
+    for (my $row = 1; $row <= @array_rows -1; $row++){
+        
+        @array_columns = split("\t",$array_rows[$row]);
+
+
+        $r_old = $r_new;
+        $r_new = $array_columns[0];
+        $dr = $r_new - $r_old;
+
+        $Sum_dr = $r_new; # the variable r is the depth into the cloud. It is the total r, so it represents the sum of all drs.
+
+        for(my $col = 1; $col <= @array_columns -1;$col++){
+            $SumProduct_dr_emissivity[$col] += $dr * $array_columns[$col];
+        }
+    }
+
+    for (my $col = 1; $col <= @SumProduct_dr_emissivity; $i++){
+        $AverageEmissivity[$col] = $SumProduct_dr_emissivity[$col] / $Sum_dr;
+    }
+    return(join("\t",@AverageEmissivity))
 }
