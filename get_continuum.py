43609ec733715cf9b77b05c369268f2394f21838
Eric Pellegrini <eric.pellegrini@uni-heidelberg.de>
2016-10-12 18:45:47 +0200

    Create place holder files for  get_line_profile and get_continuum

---
+++ get_continuum.py
@@ -0,0 +1,5 @@
+Place holder for continuum integration.
+
+1. Diffuse (default with option for total)
+2. Do specific bands (crop data)?
+3. Can't run with temperature set if it also sets the dust temperature (which Tgas != Tdust)
