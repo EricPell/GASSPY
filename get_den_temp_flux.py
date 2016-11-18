e2c4baf262227f2d79af0676dc4f395fd4670b51
Eric Pellegrini <eric.pellegrini@uni-heidelberg.de>
2016-11-18 18:05:05 +0100

    added import statement for yt

--- get_den_temp_flux.py
+++ get_den_temp_flux.py
@@ -16,6 +16,7 @@ except:
     
 
 """ Load dependencies """
+import yt
 from yt.config import ytcfg;ytcfg["yt","__withinreason"]="True"
 import matplotlib
 # Force matplotlib to not use any Xwindows backend.
