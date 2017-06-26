a565fea5a9a53cf33d9a2adfe1bd3b2e85351ed1
Eric Pellegrini <eric.pellegrini@uni-heidelberg.de>
2017-06-26 15:05:27 +0200

    updated line labels for C17

--- myconfig-example.py
+++ myconfig-example.py
@@ -11,6 +11,36 @@ mask_parameters_dict={
 opiate_library = 'silcc-combined-ems.tbl'
 opiate_lookup  = 'silcc.unique_parameters'
 
+line_labels = ["N  2      121.767m", "N  2      205.244m", "N  2      6583.45A", "Blnd      5755.00A",\
+               "O  3      88.3323m", "O  3      5006.84A", "Blnd      4363.00A",\
+               "O  2      3726.03A", "Blnd      3727.00A", "O  2      3728.81A", "Blnd      7323.00A", "Blnd      7332.00A",\
+               "O  1      63.1679m", "O  1      145.495m", "Blnd      6300.00A",\
+               "S  3      18.7078m", "S  3      33.4704m", "S  3      9530.62A", "S  3      9068.62A", "S  3      6312.06A",\
+               "S  2      6716.44A", "S  2      6730.82A", "Blnd      6720.00A",\
+               "H  1      6562.81A", "H  1      4861.33A",\
+               "C  2      157.636m",\
+               "C  1      370.269m", "C  1      609.590m",\
+               "Blnd      2.12100m",\
+               "CO        2600.05m",\
+               "CO        1300.05m",\
+               "CO        866.727m",\
+               "CO        650.074m",\
+               "CO        520.089m",\
+               "CO        433.438m",\
+               "CO        371.549m",\
+               "CO        325.137m",\
+               "CO        289.041m",\
+               "CO        260.169m",\
+               "CO        236.549m",\
+               "CO        216.868m",\
+               "CO        200.218m",\
+               "HCO+      373.490m",\
+               "HCN       375.844m",\
+               "Si 2      34.8046m"]
+
+CLOUDY_INIT_FILE = "silcc_flash_postprocess.ini"
+
+# Old line labels from pre-2017
 line_labels = ["N  2 205.4m", "N  2  6584A", "N  2  5755A",\
                "O  3 88.33m", "O  3  5007A", "TOTL  4363A",\
                "O II  3729A", "O II  3726A", "O II  7323A", "O II  7332A", "TOTL  3727A",\
@@ -20,5 +50,3 @@ line_labels = ["N  2 205.4m", "N  2  6584A", "N  2  5755A",\
                "H  1  6563A", "H  1  4861A",\
                "CO    1300m", "CO    2600m", "CO   866.7m", "CO   371.5m",\
                "SI 2 34.81m"]
-
-CLOUDY_INIT_FILE = "silcc_flash_postprocess.ini"
