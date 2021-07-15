658b36c988816731c042477258069769d4cb5209
Eric Pellegrini <ericpellegrini@outlook.com>
2021-07-15 18:24:44 +0200

    Added utility functions to save data to various fits files

---
+++ opiate/utils/save_to_multiext_fits.py
@@ -0,0 +1,24 @@
+from astropy.io import fits
+import yaml
+import numpy as np
+from opiate.utils import savename
+import glob
+import os
+
+def run(search_string, savesfile, overwrite=True):
+    file_list = sorted(glob.glob(search_string))
+    file_list.sort(key=os.path.getmtime)
+
+    data = np.load(file_list[0])
+    hdu_list = [fits.PrimaryHDU(data)] 
+
+    for file in file_list[1:]:
+        data = np.load(file)
+        hdu_list.append(fits.ImageHDU(data))
+
+    hdul = fits.HDUList(hdu_list)
+    hdul.writeto(savesfile, overwrite=overwrite)
+
+if __name__ == "__main__":
+    import sys
+    run(sys.argv[1], sys.argv[2])
