4cc2443900ab86c1bc6bfcc173ae85320707bd9a
Eric Pellegrini <ericpellegrini@outlook.com>
2021-07-16 22:28:45 +0200

    fixed save name format

--- opiate/utils/savename.py
+++ opiate/utils/savename.py
@@ -1,9 +1,15 @@
 
 
-def get_filename(line, sim_data, obs_plane):
-    # first remove all stray spaces and replace with _, removing duplicates
-    filename = line.replace(" ","_").replace("__","_")
+from numpy.lib.npyio import save
+
+
+def get_filename(line, sim_data, obs_plane, saveprefix = None):
+    if saveprefix is not None :
+        # first remove all stray spaces and replace with _, removing duplicates
+        filename = saveprefix+"_"+line.replace(" ","_").replace("__","_")
+    else:
+        filename = line.replace(" ","_").replace("__","_")
     #add rotation angles to 3 decimal precision 
-    filename += "_x0_%03.3f_%03.3f_%03.3f_pitch_%03.3f_yaw_%03.3f_roll_%03.3f"%(obs_plane.xp0_s, obs_plane.yp0_s, obs_plane.zp0_s, 
+    filename += "_x0_%07.3f_%07.3f_%07.3f_pitch_%07.3f_yaw_%07.3f_roll_%07.3f"%(obs_plane.xp0_s, obs_plane.yp0_s, obs_plane.zp0_s, 
                                                                            obs_plane.pitch, obs_plane.yaw, obs_plane.roll)
     return filename
