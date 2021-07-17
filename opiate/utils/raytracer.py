eb1f1a8dfffe86efc09f0c8a5066aad68f3111bc
Eric Pellegrini <ericpellegrini@outlook.com>
2021-07-17 20:14:57 +0200

    restructured ray trace

--- opiate/utils/raytracer.py
+++ opiate/utils/raytracer.py
@@ -25,50 +25,78 @@ def coordinate_transform(ray_df, rotation_matrix, ray_parameters_df):
         #If matrix is in system memory, move it to the GPU
         rotation_matrix = cupy.array(rotation_matrix)
 
-    ray_df["xi"] = (((ray_df["xp"] + ray_parameters_df["xp0_r"].iloc[0])*float(rotation_matrix[0][0])) +
-                    ((ray_df["yp"] + ray_parameters_df["yp0_r"].iloc[0])*float(rotation_matrix[0][1])) + 
-                    ((ray_df["zp"] + ray_parameters_df["zp0_r"].iloc[0])*float(rotation_matrix[0][2])) + ray_parameters_df["rot_origin_x"].iloc[0]).astype(int) 
+    ray_df["xi"] = cupy.full(len(ray_df), ray_parameters_df["xp0_r"].iloc[0] * float(rotation_matrix[0][0]) + 
+                                          ray_parameters_df["yp0_r"].iloc[0] * float(rotation_matrix[0][1]) +
+                                          ray_parameters_df["zp0_r"].iloc[0] * float(rotation_matrix[0][2]) + ray_parameters_df["rot_origin_x"].iloc[0])
+    ray_df["xi"] += ((ray_df["xp"]*float(rotation_matrix[0][0])) + 
+                     (ray_df["yp"]*float(rotation_matrix[0][1])) + 
+                     (ray_df["zp"]*float(rotation_matrix[0][2])))
+    ray_df["xi"] = ray_df["xi"].astype(int)
 
-    ray_df["yi"] = (((ray_df["xp"] + ray_parameters_df["xp0_r"].iloc[0])*float(rotation_matrix[1][0])) + 
-                    ((ray_df["yp"] + ray_parameters_df["yp0_r"].iloc[0])*float(rotation_matrix[1][1])) + 
-                    ((ray_df["zp"] + ray_parameters_df["zp0_r"].iloc[0])*float(rotation_matrix[1][2])) + ray_parameters_df["rot_origin_y"].iloc[0]).astype(int)
+    ray_df["yi"] = cupy.full(len(ray_df), ray_parameters_df["xp0_r"].iloc[0] * float(rotation_matrix[1][0]) + 
+                                          ray_parameters_df["yp0_r"].iloc[0] * float(rotation_matrix[1][1]) +
+                                          ray_parameters_df["zp0_r"].iloc[0] * float(rotation_matrix[1][2]) + ray_parameters_df["rot_origin_y"].iloc[0])
+    ray_df["yi"] += ((ray_df["xp"]*float(rotation_matrix[1][0])) + 
+                     (ray_df["yp"]*float(rotation_matrix[1][1])) + 
+                     (ray_df["zp"]*float(rotation_matrix[1][2])))
+    ray_df["yi"] = ray_df["yi"].astype(int)
 
-    ray_df["zi"] = (((ray_df["xp"] + ray_parameters_df["xp0_r"].iloc[0])*float(rotation_matrix[2][0])) + 
-                    ((ray_df["yp"] + ray_parameters_df["yp0_r"].iloc[0])*float(rotation_matrix[2][1])) + 
-                    ((ray_df["zp"] + ray_parameters_df["zp0_r"].iloc[0])*float(rotation_matrix[2][2])) + ray_parameters_df["rot_origin_z"].iloc[0]).astype(int)
+    ray_df["zi"] = cupy.full(len(ray_df), ray_parameters_df["xp0_r"].iloc[0] * float(rotation_matrix[2][0]) + 
+                                          ray_parameters_df["yp0_r"].iloc[0] * float(rotation_matrix[2][1]) +
+                                          ray_parameters_df["zp0_r"].iloc[0] * float(rotation_matrix[2][2]) + ray_parameters_df["rot_origin_z"].iloc[0])
+    ray_df["zi"] += ((ray_df["xp"]*float(rotation_matrix[2][0])) + 
+                     (ray_df["yp"]*float(rotation_matrix[2][1])) + 
+                     (ray_df["zp"]*float(rotation_matrix[2][2])))
+    ray_df["zi"] = ray_df["zi"].astype(int)
+
+    # ray_df["yi"] = (((ray_df["xp"] + ray_parameters_df["xp0_r"].iloc[0])*float(rotation_matrix[1][0])) + 
+    #                 ((ray_df["yp"] + ray_parameters_df["yp0_r"].iloc[0])*float(rotation_matrix[1][1])) + 
+    #                 ((ray_df["zp"] + ray_parameters_df["zp0_r"].iloc[0])*float(rotation_matrix[1][2])) + ray_parameters_df["rot_origin_y"].iloc[0])
+    # ray_df["yi"] = ray_df["yi"].astype(int)
+
+    # ray_df["zi"] = (((ray_df["xp"] + ray_parameters_df["xp0_r"].iloc[0])*float(rotation_matrix[2][0])) + 
+    #                 ((ray_df["yp"] + ray_parameters_df["yp0_r"].iloc[0])*float(rotation_matrix[2][1])) + 
+    #                 ((ray_df["zp"] + ray_parameters_df["zp0_r"].iloc[0])*float(rotation_matrix[2][2])) + ray_parameters_df["rot_origin_z"].iloc[0])
+    # ray_df["zi"] = ray_df["zi"].astype(int)
+
     pass
+def coordinate_toIndex(ray_df, ray_parameters_df):
+    ray_df["index1D"] = cudf.Series(ray_df["xi"].rmul(ray_parameters_df["Nymax"].iloc[0]*ray_parameters_df["Nzmax"].iloc[0]).radd(ray_df["yi"].rmul(ray_parameters_df["Nzmax"].iloc[0])).radd(ray_df["zi"]))
     
 def path_rayCell(ray_df, ray_parameters_df):
     """
         Takes the ray segment in the simulation coordinate frame as ints, which defines a cell.
         Using duplicate values we count the number of ray subsegments in the cells, merging subsegments corresponding to the same cells and rays.
     """
-    last = ray_df.drop_duplicates(subset=['xp', 'yp', 'xi', 'yi', 'zi'],keep="last").index.to_array()
-    ray_df.drop_duplicates(subset=['xp', 'yp', 'xi', 'yi', 'zi'], inplace=True)
-    first = ray_df.index.to_array()
-    nray_subsec = last - first
-    del(first,last)
-
-    ray_df["Nraysubseg"] = nray_subsec
-    del(nray_subsec)
-    
     query_string  = "(xi >= 0 and xi < {0} and yi >= 0 and yi < {1} and zi >= 0 and zi < {2})".format(ray_parameters_df["Nxmax"].iloc[0],ray_parameters_df["Nymax"].iloc[0],ray_parameters_df["Nzmax"].iloc[0])
     ray_df = ray_df.query(query_string)
+    # nothing in box
     if len(ray_df) == 0:
           return ray_df
+    
+    #get 1 dimensional cell index
+    coordinate_toIndex(ray_df, ray_parameters_df)
+    last = ray_df.drop_duplicates(subset=['xp', 'yp', 'index1D'],keep="last").index.to_array()
+    ray_df.drop_duplicates(subset=['xp', 'yp', 'index1D'], inplace=True)
+    #last = ray_df.drop_duplicates(subset=['xp', 'yp', 'xi', 'yi', 'zi'],keep="last").index.to_array()
+    #ray_df.drop_duplicates(subset=['xp', 'yp', 'xi', 'yi', 'zi'], inplace=True)
+    first = ray_df.index.to_array()
+    nray_subsec = last - first
+    #del(first,last)
+
+    ray_df["Nraysubseg"] = nray_subsec
+    del(nray_subsec)    
+
     ray_df["path_length"] = ray_df.Nraysubseg.rtruediv(ray_parameters_df["z_subsamples"].iloc[0])
 
     return(ray_df)
 
 
 
-def coordinate_toIndex(ray_df, ray_parameters_df):
-    ray_df["index1D"] = cudf.Series(ray_df["xi"].rmul(ray_parameters_df["Nymax"].iloc[0]*ray_parameters_df["Nzmax"].iloc[0]).radd(ray_df["yi"].rmul(ray_parameters_df["Nzmax"].iloc[0])).radd(ray_df["zi"]))
-
 def getSubphysicsIndex(ray_df, idf):
     ray_df["opiate_j"] = idf.iloc[(ray_df["xi"].values.ravel().tolist(), ray_df["yi"].values.ravel().tolist(), ray_df["zi"].values.ravel().tolist()),:] 
 
-def traceRays(sim_data, obsplane, line_labels=None, dZslab = 52):
+def traceRays(sim_data, obsplane, line_labels=None, dZslab = 52, savefiles = True, saveprefix=None):
     if line_labels is None:
         """Try and read from simdata config"""
         line_labels = sim_data.config_yaml["line_labels"]
@@ -141,7 +169,7 @@ def traceRays(sim_data, obsplane, line_labels=None, dZslab = 52):
             continue
     
         # WE NOW CONVERT COORDINATE into a 1d raveled equivlant index from 3d
-        coordinate_toIndex(raydf, ray_parameters_df=ray_parameters_df)
+        # coordinate_toIndex(raydf, ray_parameters_df=ray_parameters_df)
 
         # Clean the object and reduce memory footprint getting rid of unused coordinates
         raydf.drop(columns=["xi","yi","zi","Nraysubseg"], inplace=True)
@@ -157,11 +185,13 @@ def traceRays(sim_data, obsplane, line_labels=None, dZslab = 52):
 
         del(raydf)    
 
-    os.makedirs("%s/glowviz_output/"%(sim_data.datadir), exist_ok=True)
-    for line_label in line_labels:
-        flux_array = cupy.array(fluxes_df[line_label])
-        flux_array = cupy.asnumpy(flux_array)
-        fname = "%s/glowviz_output/%s.npy"%(sim_data.datadir,savename.get_filename(line_label, sim_data, obsplane))
-        print("saving " + fname)
-        np.save(fname, flux_array.reshape(obsplane.Nxp, obsplane.Nyp))
-    del(fluxes_df, flux_array, ray_parameters_df)
+    if savefiles:
+        os.makedirs("%s/glowviz_output/"%(sim_data.datadir), exist_ok=True)
+        for line_label in line_labels:
+            flux_array = cupy.array(fluxes_df[line_label])
+            flux_array = cupy.asnumpy(flux_array)
+            fname = "%s/glowviz_output/%s.npy"%(sim_data.datadir, savename.get_filename(line_label, sim_data, obsplane, saveprefix = saveprefix))
+            print("saving " + fname)
+            np.save(fname, flux_array.reshape(obsplane.Nxp, obsplane.Nyp))
+            del(flux_array)
+    del(fluxes_df, ray_parameters_df)
