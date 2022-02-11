a99f27765dc6d93775570c917ec50302e95eab5f
Eric Pellegrini <ericpellegrini@outlook.com>
2022-02-11 12:17:17 +0100

    de-hardoded variable types to make it flexible and consisteny

--- gasspy/settings/defaults.py
+++ gasspy/settings/defaults.py
@@ -1,38 +1,49 @@
+from turtle import pos
 import cupy
 
 """
     This file contains a dictonary specifying variables and their dtypes associated with the raytrace
 """
+# Default float used
+default_float = cupy.float64
+
 
 rayid_dtype = cupy.int32
-default_float = cupy.float32
+cellid_dtype = cupy.int64
+amr_lref_dtype = cupy.int16
+
+pos_dtype = default_float
+
+
 # Dtypes
 ray_dtypes = {
     # Variables specific to the ray
-    "xp" : cupy.float64, "yp" : cupy.float64,                       # x and y coordinate on the detector plane 
+    "xp" : pos_dtype, "yp" : pos_dtype,                       # x and y coordinate on the detector plane 
     "global_rayid" : rayid_dtype,                                   # unique id of the ray, common and fixed throughout the code 
     "trace_status": cupy.int8,                                      # flag for if a ray is (0)waiting/(1)active/(2)finished in its raytrace
     "pid"         : rayid_dtype,                                    # unique id of parent ray
     "pevid"       : rayid_dtype,                                    # id of the "parent split event" causing the creation of the ray
     "cevid"       : rayid_dtype,                                    # id of the "child split event" causing the termination of the ray
     "aid"        : rayid_dtype,                                    # id of the "ancestral" ray, used as the id of the branch 
-    "ray_lrefine" : cupy.int16,                                     # ray refinement level
-    "refinement_level" : cupy.int8,                                 # TODO: DEPRICATED - REMOVE
+    "ray_lrefine" : amr_lref_dtype,                                     # ray refinement level
     
     # Variables specific to the current location of the ray
-    "xi" : cupy.float64, "yi" : cupy.float64, "zi" : cupy.float64,                # x,y and z coordinates in the simulation domain
-    "raydir_x": cupy.float64, "raydir_y": cupy.float64, "raydir_z": cupy.float64, # x, y, z normal directions of the ray path
-    "index1D"     : cupy.int64,                                                   # 1D raveled index of the cell currently containing the ray
-    "amr_lrefine" : cupy.int16,                                                   # local amr refinement level
-    "pathlength"  : cupy.float64,                                   # the pathlength of the ray through the cell
+    "xi" : pos_dtype, "yi" : pos_dtype, "zi" : pos_dtype,                # x,y and z coordinates in the simulation domain
+    "raydir_x": pos_dtype, "raydir_y": pos_dtype, "raydir_z": pos_dtype, # x, y, z normal directions of the ray path
+    "index1D"     : cellid_dtype,                                                   # 1D raveled index of the cell currently containing the ray
+    "next_index1D"     : cellid_dtype,                                                   # 1D raveled index of the next cell containing the ray
+    "amr_lrefine" : amr_lref_dtype,                                                   # local amr refinement level
+    "pathlength"  : pos_dtype,                                   # the pathlength of the ray through the cell
     "ray_status": cupy.int8,                                        # flag for the ray being (0)fine/(1)filled its buffer/(2)terminated for leaving the domain
                                                                     # /(3)split due to refinement
-    "active_rayDF_to_buffer_map" : cupy.int32,                      # Mapping index from the active_rayDF to the 2D buffer arrays
+    "active_rayDF_to_buffer_map" : rayid_dtype,                      # Mapping index from the active_rayDF to the 2D buffer arrays
     "buffer_current_step": cupy.int16,                              # Current cell in the buffer
     "dump_number" : cupy.int16,                                     # Number of dumps the current ray has done
-    "ray_area"    : cupy.float64,                                    # current area covered by the solid angle of the ray
+    "ray_area"    : pos_dtype,                                    # current area covered by the solid angle of the ray
     # Variables specific to the buffer
-    "buff_slot_occupied":cupy.int8                                  # Flag if a spot in the buffer is occupied 
+    "buff_slot_occupied":cupy.int8,                                  # Flag if a spot in the buffer is occupied 
+    
+    "cell_index": cellid_dtype                                       # Index of the cell in the raveled list supplied by the user
 }
 
 
@@ -54,16 +65,22 @@ ray_defaults = {
     "xi" : -1, "yi" : -1, "zi" : -1,
     "raydir_x": -1, "raydir_y": -1, "raydir_z": -1,
     "index1D"     : -1,
+    "next_index1D"     : -1,
     "amr_lrefine" : -1,
     "pathlength"  : 0,
     "ray_status": 0,
                                                            
     "active_rayDF_to_buffer_map" : -1,
     "buffer_current_step": 0,
-    "dump_number" : 0,
+    "dump_number" : -1,
     "ray_area"    : 0,
 
     # Variables specific to the buffer
-    "buff_slot_occupied": 0
+    "buff_slot_occupied": 0,
 
+    # Default maximum and minimum refinement levels for the rays
+    "ray_lrefine_min" : 1,
+    "ray_lrefine_max" : 30,
+    
+    "cell_index" : -1
 }
