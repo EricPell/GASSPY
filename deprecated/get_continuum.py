5b00df5cdf1aaa6821d50589b5a7d9ad26a90095
Eric Pellegrini <ericpellegrini@outlook.com>
2022-02-11 16:49:19 +0100

    Delete vestigal opiate files

--- deprecated/get_continuum.py
+++
@@ -1,5 +0,0 @@
-Place holder for continuum integration.
-
-1. Diffuse (default with option for total)
-2. Do specific bands (crop data)?
-3. Can't run with temperature set if it also sets the dust temperature (which Tgas != Tdust)
