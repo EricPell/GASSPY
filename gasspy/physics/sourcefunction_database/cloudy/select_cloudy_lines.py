e526143876ec225a7d79e9cd12547c7c9c6ebc26
Eric Pellegrini <ericpellegrini@outlook.com>
2022-02-13 15:32:28 +0100

    Helper function to provide emission line labels

---
+++ gasspy/physics/sourcefunction_database/cloudy/select_cloudy_lines.py
@@ -0,0 +1,134 @@
+"""Import line label alias"""
+
+class C17(object):
+    NII_121   = "N  2 121.767m"
+    NII_205   = "N  2 205.244m"
+    NII       = "N  2 6583.45A"
+    NII_5755  = "Blnd 5755.00A"
+    OIII_88   = "O  3 88.3323m"
+    OIII      = "O  3 5006.84A"
+    OIII_4363 = "Blnd 4363.00A"
+    OII       = "Blnd 3727.00A"
+    OII_3726  = "O  2 3726.03A"
+    OII_3729  = "O  2 3728.81A"
+    OII_7323  = "Blnd 7323.00A"
+    OII_7332  = "Blnd 7332.00A"
+    OI_63     = "O  1 63.1679m"
+    OI_145    = "O  1 145.495m"
+    OI_6300   = "Blnd 6300.00A"
+    SIV       = "S  4 10.5076m"
+    SIII_18   = "S  3 18.7078m"
+    SIII_33   = "S  3 33.4704m"
+    SIII_9530 = "S  3 9530.62A"
+    SIII_9068 = "S  3 9068.62A"
+    SIII_6312 = "S  3 6312.06A"
+    SII_6716  = "S  2 6716.44A"
+    SII_6731  = "S  2 6730.82A"
+    SII       = "Blnd 6720.00A"
+    NeV_14    = "Ne 5 14.3228m"
+    NeV_24    = "Ne 5 24.2065m"
+    NeIII     = "Ne 3 3868.76A"
+    NeIII_15  = "Ne 3 15.5509m"
+    NeIII_36  = "Ne 3 36.0036m"
+    NeII_12   = "Ne 2 12.8101m"
+    ArIII_9   = "Ar 3 8.98898m"
+    ArIII_21  = "Ar 3 21.8253m"
+    ArII_7    = "Ar 2 6.98337m"
+    Ha        = "H  1 6562.81A"
+    Hb        = "H  1 4861.33A"
+    CII       = "C  2 157.636m"
+    CI_370    = "C  1 370.269m"
+    CI_609    = "C  1 609.590m"
+    H2_212    = "Blnd 2.12100m"
+    CO_1_0    = "CO   2600.05m"
+    CO_2_1    = "CO   1300.05m"
+    CO_3_2    = "CO   866.727m"
+    CO_4_3    = "CO   650.074m"
+    CO_5_4    = "CO   520.089m"
+    CO_6_5    = "CO   433.438m"
+    CO_7_6    = "CO   371.549m"
+    CO_8_7    = "CO   325.137m"
+    CO_9_8    = "CO   289.041m"
+    CO_10_9   = "CO   260.169m"
+    CO_11_10  = "CO   236.549m"
+    CO_12_11  = "CO   216.868m"
+    CO_13_12  = "CO   200.218m"
+    HCOp      = "HCO+ 373.490m"
+    HCN       = "HCN  375.844m"
+    SiII      = "Si 2 34.8046m"
+    H2        = "H2   2.12125m"
+    H2_S0     = "H2   5.50996m"
+    H2_S1     = "H2   6.10718m"
+    H2_S2     = "H2   6.90725m"
+    H2_S3     = "H2   8.02362m"
+    H2_S4     = "H2   9.66228m"
+    H2_S5     = "H2   12.2752m"
+    H2_S6     = "H2   17.0300m"
+    H2_S7     = "H2   28.2130m"
+
+class labels(object):
+    line = {
+    C17.NII_121   : "[NII] 122m",
+    C17.NII_205   : "[NII] 205m",
+    C17.NII       : "[NII] 6584A",
+    C17.NII_5755  : "[NII] 5755A",
+    C17.OIII_88   : "[OIII] 88m",
+    C17.OIII      : "[OIII] 5007A",
+    C17.OIII_4363 : "[OIII] 4363A",
+    C17.OII       : "[OII] 3727A",
+    C17.OII_3726  : "[OII] 3726A",
+    C17.OII_3729  : "[OII] 3729A",
+    C17.OII_7323  : "[OII] 7323A",
+    C17.OII_7332  : "[OII] 7332A",
+    C17.OI_63     : "[OI] 63m",
+    C17.OI_145    : "[OI] 145m",
+    C17.OI_6300   : "[OI] 6300A",
+    C17.SIV       : "[SIV] 10.5m",
+    C17.SIII_18   : "[SIII] 18m",
+    C17.SIII_33   : "[SIII] 33.5m",
+    C17.SIII_9530 : "[SIII] 9531A",
+    C17.SIII_9068 : "[SIII] 9069A",
+    C17.SIII_6312 : "[SIII] 6312A",
+    C17.SII_6716  : "[SII] 6716",
+    C17.SII_6731  : "[SII] 6731" , 
+    C17.SII       : "[SII] 6720A",
+    C17.NeV_14    : "[NeV] 14m",
+    C17.NeV_24    : "[NeV] 24m",
+    C17.NeIII_15  : "[NeIII] 15m",
+    C17.NeIII_36  : "[NeIII] 36m",
+    C17.NeII_12   : "[NeII] 12.8m",
+    C17.ArIII_9   : "[ArIII] 9m",
+    C17.ArIII_21  : "[ArIII] 21.8m",
+    C17.ArII_7    : "[ArII] 6.9m",
+    C17.Ha        : "Halpha",
+    C17.Hb        : "Hbeta",
+    C17.CII       : "[CII] 158m",
+    C17.CI_370    : "[CI] 370m",
+    C17.CI_609    : "[CI] 609m",
+    C17.H2_212    : "H2 2.121m",
+    C17.CO_1_0    : "CO J(1-0)",
+    C17.CO_2_1    : "CO J(2-1)",
+    C17.CO_3_2    : "CO J(3-2)",
+    C17.CO_4_3    : "CO J(4-3)",
+    C17.CO_5_4    : "CO J(5-4)",
+    C17.CO_6_5    : "CO J(6-5)",
+    C17.CO_7_6    : "CO J(7-6)",
+    C17.CO_8_7    : "CO J(8-7)",
+    C17.CO_9_8    : "CO J(9-8)",
+    C17.CO_10_9   : "CO J(10-9)",
+    C17.CO_11_10  : "CO J(11-10)",
+    C17.CO_12_11  : "CO J(12-11)",
+    C17.CO_13_12  : "CO J(13-120)",
+    C17.HCOp      : "HCO+ 373.490m",
+    C17.HCN       : "HCN  375.844m",
+    C17.SiII      : "[SI 2] 34m",
+    C17.H2        : "H2 2.121m",
+    C17.H2_S0     : "H2 S(0) 5.50m",
+    C17.H2_S1     : "H2 S(1) 6.1m",
+    C17.H2_S2     : "H2 S(2) 6.9m",
+    C17.H2_S3     : "H2 S(3) 8.0m",
+    C17.H2_S4     : "H2 S(4) 9.6m",
+    C17.H2_S5     : "H2 S(5) 12.2m",
+    C17.H2_S6     : "H2 S(6) 17.0m",
+    C17.H2_S7     : "H2 S(7) 28.2m"
+}
