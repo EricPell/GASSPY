5f88fa1160f0e489e6672664bfbc28e81f0282fd
LokeOhlin <loke.lonnblad@gmail.com>
2022-03-03 15:27:14 +0100

    Fixed issue of non matching shapes

--- gasspy/shared_utils/spectra_functions/continuum.py
+++ gasspy/shared_utils/spectra_functions/continuum.py
@@ -10,4 +10,4 @@ def continuum_linear_interp(Energies, fluxes, idx0 = 0, idx1 = -1, numlib = np):
     E0 = Energies[idx0]
     E1 = Energies[idx1]
     delta = (f1 - f0) / (E1 - E0)
-    return f0 + delta * (Energies[numlib.newaxis, :] - E0)
+    return f0[:,numlib.newaxis] + delta[:,numlib.newaxis] * (Energies[numlib.newaxis, :] - E0)
