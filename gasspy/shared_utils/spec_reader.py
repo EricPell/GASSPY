846de84a5bf0f7df5cd404ee10eefa05d7a25f71
LokeOhlin <loke.lonnblad@gmail.com>
2022-07-19 16:40:53 +0200

    Made map creation faster by avoiding loop over indiviual rays

--- gasspy/shared_utils/spec_reader.py
+++ gasspy/shared_utils/spec_reader.py
@@ -7,11 +7,10 @@ from pathlib import Path
 import sys
 
 from gasspy.shared_utils.spectra_functions import broadband, integrated_line
-
-
+from gasspy.io import gasspy_io
 class spec_reader:
     def __init__(self, spec_file,
-        root_dir="./",
+        sim_dir="./",
         gasspy_subdir="GASSPY",
         gasspy_spec_subdir="spec",
         maxmem_GB = None,
@@ -24,7 +23,7 @@ class spec_reader:
             gasspy_spec_subdir : string (name of the spectra subdirectory within GASSPY sub-directory)
             maxmem_GB: float (maximum amount of system memory we will try to use. NOTE: this is just and estimate so be conservative)
         """
-        self.root_dir = root_dir
+        self.sim_dir = sim_dir
         self.gasspy_subdir = gasspy_subdir
         self.gasspy_spec_subdir = gasspy_spec_subdir
         if not isinstance(spec_file, h5py._hl.files.File):
@@ -33,17 +32,20 @@ class spec_reader:
                 spec_file += ".hdf5"
             if Path(spec_file).is_file():
                 tmp_path = spec_file
-            elif Path(self.root_dir+self.gasspy_subdir+self.gasspy_spec_subdir+spec_file).is_file():
-                tmp_path = self.root_dir+self.gasspy_subdir+self.gasspy_spec_subdir+spec_file
+            elif Path(self.sim_dir+self.gasspy_subdir+self.gasspy_spec_subdir+spec_file).is_file():
+                tmp_path = self.sim_dir+self.gasspy_subdir+self.gasspy_spec_subdir+spec_file
             else:
                 sys.exit("Could not find the traced rays file\n"+\
                 "Provided path: %s"%spec_file+\
-                "Try looking in \"./\" and %s\n"%(self.root_dir+self.gasspy_spec_subdir)+\
+                "Try looking in \"./\" and %s\n"%(self.sim_dir+self.gasspy_spec_subdir)+\
                 "Aborting...")            
 
             self.spec_file = h5py.File(tmp_path, "r")
         else:
             self.spec_file = spec_file
+
+        ## Load the gasspy_config yaml
+        gasspy_config = gasspy_io.read_fluxdef(self.sim_dir+"/gasspy_config.yaml")
 
         # Load the posional information of the rays
         self.xp = self.spec_file["x"][:]
@@ -66,8 +68,8 @@ class spec_reader:
             self.maxmem = psutil.virtual_memory().free - 0.5*1024**3
 
         # TODO: This should be save parameters both inside of traced rays and spec
-        self.observer_size_x = np.max(self.xp + 0.5/2**self.ray_lrefine.astype(float)) - np.min(self.xp - 0.5/2**self.ray_lrefine.astype(float))
-        self.observer_size_y = np.max(self.yp + 0.5/2**self.ray_lrefine.astype(float)) - np.min(self.yp - 0.5/2**self.ray_lrefine.astype(float))
+        self.observer_size_x = gasspy_config["detector_size_x"]
+        self.observer_size_y = gasspy_config["detector_size_y"]
 
     def read_rays(self, idxs, Eidxs):
         if isinstance(idxs, int):
@@ -118,10 +120,10 @@ class spec_reader:
         # If nx or ny are not defined we use the maximum of the providied rays
         outmap_ray_lrefine = np.max(self.ray_lrefine)
         if outmap_nx is None:
-            outmap_nx = int(2**outmap_ray_lrefine  * outmap_size_x/self.observer_size_x)
+            outmap_nx = np.around(2**outmap_ray_lrefine  * outmap_size_x/self.observer_size_x).astype(int)
         if outmap_ny is None:
-            outmap_ny = int(2**outmap_ray_lrefine  * outmap_size_y/self.observer_size_y)
-
+            outmap_ny = np.around(2**outmap_ray_lrefine  * outmap_size_y/self.observer_size_y).astype(int)
+        print(outmap_nx, outmap_ny)
         # Size of each pixel 
         outmap_dx = outmap_size_x/outmap_nx
         outmap_dy = outmap_size_y/outmap_ny
@@ -152,11 +154,15 @@ class spec_reader:
         # Treat each ray refinement level seperatly
         for lref in range(np.min(self.ray_lrefine), np.max(self.ray_lrefine) +1):
             # Size of rays at this refinement level
-            ray_dx = self.observer_size_x / 2**lref
-            ray_dy = self.observer_size_y / 2**lref
-            ray_nx = ray_dx/outmap_dx
-            ray_ny = ray_dy/outmap_dy
-            print(lref, ray_ny, ray_nx)
+            ray_dx = self.observer_size_x / 2**float(lref)
+            ray_dy = self.observer_size_y / 2**float(lref)
+            ray_nx = int(ray_dx/outmap_dx)
+            if ray_dx/outmap_dx > ray_nx:
+                ray_nx += 1
+            ray_ny = int(ray_dy/outmap_dy)
+            if ray_dy/outmap_dy > ray_ny:
+                ray_ny += 1
+            print(lref, ray_ny, ray_nx, ray_dx, ray_dy, outmap_dx, outmap_dy)
 
             idxs_at_lref = idxs[np.where(self.ray_lrefine[idxs] == lref)]
             nray_remaining = len(idxs_at_lref)
@@ -178,66 +184,94 @@ class spec_reader:
                 map_ystart = self.yp[idxs_now] - 0.5*ray_dy - ylims[0]
                 map_yend   = self.yp[idxs_now] + 0.5*ray_dy - ylims[0]           
 
-                map_ixstart = (map_xstart/outmap_dx).astype(int)
-                map_ixend   = (map_xend/outmap_dx).astype(int)
+                map_ixstart = np.floor(map_xstart/outmap_dx).astype(int)
+                map_ixend   = np.ceil(map_xend/outmap_dx).astype(int)
 
-                map_iystart = (map_ystart/outmap_dy).astype(int)
-                map_iyend   = (map_yend/outmap_dy).astype(int)
+                map_iystart = np.floor(map_ystart/outmap_dy).astype(int)
+                map_iyend   = np.ceil(map_yend/outmap_dy).astype(int)
 
+                ## Bounds
+                map_ixstart[map_ixstart<0] = 0
+                map_iystart[map_iystart<0] = 0
+
+                map_ixend[map_ixend >= outmap_nx] = outmap_nx-1
+                map_iyend[map_iyend >= outmap_ny] = outmap_ny-1
 
                 #determine left and right covering fraction
                 fxleft  = ((map_ixstart + 1)*outmap_dx - map_xstart)
                 fxright = (map_xend - map_ixend*outmap_dx )
                 fyleft  = ((map_iystart + 1)*outmap_dy - map_ystart)
                 fyright = (map_yend - map_iyend*outmap_dy)
-                
+
                 fxleft[fxleft < 0 ] = 0
                 fyleft[fyleft < 0 ] = 0
                 fxright[fxright < 0 ] = 0
                 fyright[fyright < 0 ] = 0
+            
+                fxleft[fxleft > outmap_dx ] = outmap_dx
+                fyleft[fyleft > outmap_dy ] = outmap_dy
+                fxright[fxright > outmap_dx ] = outmap_dx
+                fyright[fyright > outmap_dy ] = outmap_dy
 
-
+                #print("x", map_ixstart[0], map_xstart[0]/outmap_dx, map_ixend[0], map_xend[0]/outmap_dx, fxleft[0]/outmap_dx, fxright[0]/outmap_dx, fxright[0])
+                #print("y", map_iystart[0], map_ystart[0]/outmap_dy, map_iyend[0], map_yend[0]/outmap_dy, fyleft[0]/outmap_dy, fyright[0]/outmap_dy)
+ 
                 # If rays are large enough to fully cover more than one map cell, we need to deal with these seperatly. 
                 # TODO: Currently we need to loop over rays for this, but we should find a way to avoid this loop
-                if ray_nx >= 2 or ray_ny >= 2: 
-                    for iray in range(nrays_now):
-                        # Cells completely covered by the ray
+                #if ray_nx >= 2 or ray_ny >= 2: 
+                nx_rays = map_ixend - map_ixstart + 1
+                ny_rays = map_ixend - map_ixstart + 1
+
+                for nx_now in range(np.min(nx_rays), np.max(nx_rays)+1):
+                    with_nx = nx_rays == nx_now
+                    for ny_now in range(np.min(ny_rays), np.max(ny_rays)+1):
+                        rays_now = np.where(with_nx*(ny_rays==ny_now))[0]
+                        if len(rays_now) == 0:
+                            continue
+                        ixp , iyp = np.meshgrid(np.arange(0,nx_now), np.arange(0,ny_now), indexing = "ij")
+                        ixp = ixp.ravel()
+                        iyp = iyp.ravel()
+                        ixps = (map_ixstart[rays_now][:,np.newaxis] + ixp[np.newaxis,:]).ravel()
+                        iyps = (map_iystart[rays_now][:,np.newaxis] + iyp[np.newaxis,:]).ravel()
+
+                        in_map = (ixps <outmap_nx) * (iyps < outmap_ny)
+
+                        
+                        fxl = np.maximum(np.minimum(((ixps+1)*outmap_dx-np.repeat(map_xstart[rays_now],nx_now*ny_now)), outmap_dx), 0)
+                        fyl = np.maximum(np.minimum(((iyps+1)*outmap_dy-np.repeat(map_ystart[rays_now],nx_now*ny_now)), outmap_dy), 0)
+                        fxr = np.maximum(np.minimum((-(ixps )*outmap_dx+np.repeat(map_xend[rays_now]  ,nx_now*ny_now)), outmap_dx), 0)
+                        fyr = np.maximum(np.minimum((-(iyps )*outmap_dy+np.repeat(map_yend[rays_now]  ,nx_now*ny_now)), outmap_dy), 0)
+                        fx = np.minimum(fxl,fxr)
+                        fy = np.minimum(fyl,fyr)
                         for ifl in range(outmap_nfields):
-                            if ray_nx >= 2 and ray_ny >= 2:
-                                outmap[map_ixstart[iray] + 1 : map_ixend[iray], map_iystart[iray] + 1: map_iyend[iray], ifl] += fields[iray, ifl]*outmap_dx*outmap_dy
-
-                            # side cells on x fully covered by y
-                            if ray_ny >= 2:
-                                if map_ixstart[iray] >= 0 :
-                                    outmap[map_ixstart[iray], map_iystart[iray] + 1: map_iyend[iray], ifl] += fields[iray, ifl] * fxleft[iray]  * outmap_dy
-                                if map_ixend[iray] < outmap.shape[0]:
-                                    outmap[map_ixend[iray]  , map_iystart[iray] + 1: map_iyend[iray], ifl] += fields[iray, ifl] * fxright[iray] * outmap_dy                        
-
-                            # side cells on y fully covered by x
-                            if ray_nx >= 2:
-                                if map_iystart[iray] >= 0 :
-                                    outmap[map_ixstart[iray] + 1: map_ixend[iray], map_iystart[iray], ifl] += fields[iray, ifl] * outmap_dx * fyleft[iray] 
-                                if map_iyend[iray] < outmap.shape[1]:                            
-                                    outmap[map_ixstart[iray] + 1: map_ixend[iray], map_iyend[iray]  , ifl] += fields[iray, ifl] * outmap_dx * fyright[iray]
-                # Corners
-                # lower left
-                inside = np.where((map_ixstart >= 0)*(map_iystart >=0))[0] 
-                outmap[map_ixstart[inside], map_iystart[inside], :] += (fields * fxleft[:,np.newaxis] * fyleft[:,np.newaxis])[inside,:]
-                # lower right
-                inside = np.where((map_ixend < outmap.shape[0])*(map_iystart >=0))[0]
-                outmap[map_ixend[inside], map_iystart[inside], : ]  += (fields * fxright[:,np.newaxis] * fyleft[:,np.newaxis])[inside,:]
-                # upper left
-                inside = np.where((map_ixstart >= 0)*(map_iyend < outmap.shape[1]))[0] 
-                outmap[map_ixstart[inside], map_iystart[inside], :] += (fields * fxleft[:,np.newaxis] * fyright[:,np.newaxis])[inside,:]
-                # upper right
-                inside = np.where((map_ixend < outmap.shape[0])*(map_iyend < outmap.shape[1]))[0]
-                outmap[map_ixend[inside], map_iyend[inside], : ]    += (fields * fxright[:,np.newaxis] * fyright[:,np.newaxis])[inside,:]                
-
-                
+                            np.add.at(outmap, (ixps[in_map], iyps[in_map], np.full(ixps.shape, ifl)[in_map]),  (np.repeat(fields[rays_now,ifl], nx_now*ny_now)*fx*fy)[in_map])
+                            #outmap[ixps, iyps, ifl] += np.repeat(fields[rays_now,ifl], nx_now*ny_now)*fx*fy
+#                for iray in range(nrays_now):
+#                    # Cells completely covered by the ray
+#                    nx_now = map_ixend[iray] - map_ixstart[iray] + 1
+#                    ny_now = map_iyend[iray] - map_iystart[iray] + 1
+#                    #if nx_now <=2 and ny_now <=2:
+#                    #    continue
+#
+#
+#                    xx = np.arange(map_ixstart[iray],map_ixend[iray]+1)
+#                    yy = np.arange(map_iystart[iray],map_iyend[iray]+1)
+#                    ixps, iyps = np.meshgrid(xx,yy,indexing = "ij")
+#                    #print(ixps.shape,nx_now, ny_now)
+#                    fxl = np.maximum(np.minimum(((ixps+1)*outmap_dx-map_xstart[iray]),np.ones((nx_now,ny_now))*outmap_dx), np.zeros((nx_now,ny_now)))
+#                    fyl = np.maximum(np.minimum(((iyps+1)*outmap_dy-map_ystart[iray]),np.ones((nx_now,ny_now))*outmap_dy), np.zeros((nx_now,ny_now)))
+#                    fxr = np.maximum(np.minimum((-(ixps )*outmap_dx+map_xend[iray]  ),np.ones((nx_now,ny_now))*outmap_dx), np.zeros((nx_now,ny_now)))
+#                    fyr = np.maximum(np.minimum((-(iyps )*outmap_dy+map_yend[iray]  ),np.ones((nx_now,ny_now))*outmap_dy), np.zeros((nx_now,ny_now)))
+#                    fx = np.minimum(fxl,fxr)
+#                    fy = np.minimum(fyl,fyr)
+#                                     
+#                    for ifl in range(outmap_nfields):
+#                        outmap[map_ixstart[iray]:map_ixend[iray]+1, map_iystart[iray]:map_iyend[iray]+1, ifl] += fields[iray,ifl]*fx*fy
                 nray_remaining = nray_remaining - nrays_now                 
 
         # Go back to surface brightness
         outmap = outmap/outmap_dx/outmap_dy 
+        #print(np.min(outmap), np.max(outmap))
         return outmap
     def read_spec(self, x, y, Elims = None, return_integrated_line = False, return_broadband = True):
         """
