8ca0da505c2ea316836efb92c327e143c80c23d2
LokeOhlin <loke.lonnblad@gmail.com>
2022-09-09 14:47:14 +0200

    Added option for prefix to progress bar

--- gasspy/shared_utils/loop_progress.py
+++ gasspy/shared_utils/loop_progress.py
@@ -2,10 +2,10 @@
 
 symbolset = ["/", "-", "\\", "|", "/", "-", "\\", "|"]
 nsymbols = len(symbolset)
-def print_progress(iter, imax, nbars = 20, start=""):
+def print_progress(iter, imax, nbars = 20, start="", end = ""):
     ibar = int((iter/imax)*nbars)
     isymbol = int((ibar + 1 - (iter/imax)*nbars)*nsymbols)%nsymbols
-    string = start + "["+"*"*ibar + symbolset[isymbol]+" "*(nbars-(ibar+1))+"]"
+    string = start + "["+"*"*ibar + symbolset[isymbol]+" "*(nbars-(ibar+1))+"]" + end
     print(string, end="\r")
     if iter == imax - 1:
         print("\n")
