522e8adfbe83c1d60fc2cbe3eab612ca1432bdea
LokeOhlin <loke.lonnblad@gmail.com>
2023-04-14 16:28:46 +0200

    1) Fixed error with wrong rank being used 2) Added print for all ranks 3) Make print flush so that its shown instantly

--- gasspy/shared_utils/mpi_utils/mpi_os.py
+++ gasspy/shared_utils/mpi_utils/mpi_os.py
@@ -6,13 +6,13 @@ mpi_rank = mpi_comm.rank
 mpi_size = mpi_comm.Get_size()
 
 def mpi_mkdir(path, mode = 0o777, dir_fd  = None):
-    if mpi_rank != 0:
+    if mpi_rank == 0:
         os.mkdir(path, mode=mode, dir_fd = dir_fd)
 
 def mpi_makedirs(path, mode = 0o777, exist_ok = False):
-    if mpi_rank != 0:
+    if mpi_rank == 0:
         os.makedirs(path, mode=mode, exist_ok = exist_ok)
 
 def mpi_copy(src, dst, follow_symlinks=True):
-    if mpi_rank != 0:
+    if mpi_rank == 0:
         shutil.copy(src, dst, follow_symlinks=True)
--- gasspy/shared_utils/mpi_utils/mpi_print.py
+++ gasspy/shared_utils/mpi_utils/mpi_print.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import sys
 mpi_comm = MPI.COMM_WORLD
 mpi_rank = mpi_comm.rank
 mpi_size = mpi_comm.Get_size()
@@ -6,3 +7,7 @@ mpi_size = mpi_comm.Get_size()
 def mpi_print(print_statement):
     if mpi_rank == 0:
         print(print_statement)
+        sys.stdout.flush()
+def mpi_all_print(print_statement):
+    print(print_statement)
+    sys.stdout.flush()
