9cb03b626bdd1bff7c382476ea9acc9cf6fdb65e
LokeOhlin <loke.lonnblad@gmail.com>
2023-04-15 14:30:47 +0200

    Changed print functions  to be proper wrappers

--- gasspy/shared_utils/mpi_utils/mpi_print.py
+++ gasspy/shared_utils/mpi_utils/mpi_print.py
@@ -4,10 +4,11 @@ mpi_comm = MPI.COMM_WORLD
 mpi_rank = mpi_comm.rank
 mpi_size = mpi_comm.Get_size()
 
-def mpi_print(print_statement):
+def mpi_print(*args, **kwargs):
     if mpi_rank == 0:
-        print(print_statement)
+        print(*args, **kwargs)
         sys.stdout.flush()
-def mpi_all_print(print_statement):
-    print(print_statement)
+        
+def mpi_all_print(*args, **kwargs):
+    print(*args, **kwargs)
     sys.stdout.flush()
