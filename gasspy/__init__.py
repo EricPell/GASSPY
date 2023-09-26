86eeadfcc263322746c322367f0a074bee3d91db
LokeOhlin <loke.lonnblad@gmail.com>
2023-09-26 10:25:29 +0200

    Added imports to __init__s

--- gasspy/__init__.py
+++ gasspy/__init__.py
@@ -1,1 +1,2 @@
 from .physics.databasing.database_creator import DatabaseCreator
+import gasspy.raytracing.observers as observers
--- gasspy/radtransfer/__init__.py
+++ gasspy/radtransfer/__init__.py
@@ -1,1 +1,2 @@
 from .__rt_branch__ import FamilyTree
+from .rt_trace import Trace_processor
