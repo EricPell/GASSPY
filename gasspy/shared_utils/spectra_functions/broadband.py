3945041b53a50942159eaae545a847eb2375065f
LokeOhlin <loke.lonnblad@gmail.com>
2023-10-11 16:06:51 +0200

    Added energies

--- gasspy/shared_utils/spectra_functions/broadband.py
+++ gasspy/shared_utils/spectra_functions/broadband.py
@@ -4,11 +4,11 @@ import cupy
 from gasspy.shared_utils.spectra_functions.continuum import continuum_linear_interp 
 
 def broadband(Energies, deltaEnergies, fluxes, numlib = np, Eaxis = 1):
-    return numlib.sum(fluxes*deltaEnergies, axis = Eaxis).reshape((len(fluxes),1))
+    return numlib.sum(fluxes*deltaEnergies/Energies, axis = Eaxis).reshape((len(fluxes),1))
 
 def continuum_removed_broadband(Energies, deltaEnergies, fluxes, numlib = np, Eaxis = 1, continuum_method = continuum_linear_interp):
     """
         Currently identical to integrated_line
     """
     continuum = continuum_method(Energies, fluxes, numlib=numlib, Eaxis = Eaxis)
-    return numlib.sum((fluxes - continuum)*deltaEnergies, axis  = Eaxis).reshape(fluxes.shape + (1,))
+    return numlib.sum((fluxes - continuum)*deltaEnergies/Energies, axis  = Eaxis).reshape(fluxes.shape + (1,))
